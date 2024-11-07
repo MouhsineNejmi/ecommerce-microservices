085fa4a7b82a7179df1be24765f983c0fee96907
MouhsineNejmi <nejmimouhsine@gmail.com>
2024-11-07 18:22:24 +0100

    create a mongo db for users and connect to it in user-ms

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, json, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -12,6 +13,19 @@ app.get('/api/users', (req: Request, res: Response) => {
   res.send('User Service');
 });
 
-app.listen(port, () => {
-  console.log(`[user-service]: Server is running at http://localhost:${port}`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://user-mongo-srv:27017/users');
+    console.log('[user-service]: Connected To MongoDB');
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(port, () => {
+    console.log(
+      `[user-service]: Server is running at http://localhost:${port}`
+    );
+  });
+};
+
+start();

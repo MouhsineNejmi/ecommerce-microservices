656a7dde0c892c0e0d6ce97157b00b958176067e
MouhsineNejmi <nejmimouhsine@gmail.com>
2024-11-07 16:18:23 +0100

    dockerize all microservices

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -1,10 +1,12 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, json, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
+
+app.use(json());
 
 app.get('/api/users', (req: Request, res: Response) => {
   res.send('User Service');
